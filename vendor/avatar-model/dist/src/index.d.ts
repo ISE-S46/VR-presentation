1d19d1b7e8edd8cc190cb648ffd71da74bce7125
Phoowagone Guergoson <gu.phoowagone_st@tni.ac.th>
2026-05-06 11:07:20 +0800

    Update model library and component usage

---
+++ vendor/avatar-model/dist/src/index.d.ts
@@ -0,0 +1,17 @@
+import { AvatarController } from './core/avatarController';
+import { TTSHandler } from './service/tts';
+export interface AvatarOptions {
+    modelUrl: string;
+    script?: string;
+    audioUrl?: string;
+    ttsEndpoint?: string | TTSHandler;
+    button?: boolean;
+    element?: HTMLElement | null;
+    event?: string;
+}
+export interface AvatarScene {
+    controller: AvatarController;
+    /** Disposes the controller AND removes any event listener added to options.element */
+    destroy: () => void;
+}
+export declare function createAvatarScene(canvas: HTMLCanvasElement, options: AvatarOptions): Promise<AvatarScene>;
